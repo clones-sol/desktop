03fd87c818dd6c00aab0728a4237d90a13e3caf9
redDwarf03 <sylvain@archethic.net>
2025-06-04 23:54:58 +0200

    Externalize postHog api key & instance url to .env.* files

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -6,10 +6,13 @@ import { browser } from '$app/environment';
 export const prerender = true;
 export const ssr = false;
 
+const POSTHOG_API_KEY = import.meta.env.VITE_POSTHOG_API_KEY;
+const POSTHOG_API_HOST = import.meta.env.VITE_POSTHOG_API_HOST;
+
 export const load = async () => {
   if (browser) {
-    posthog.init('phc_fj4VJCcM1CdHiNzhNDVi2LwClnEN1DG3zApjNiSJgld', {
-      api_host: 'https://insi.viralmind.ai',
+    posthog.init(POSTHOG_API_KEY, {
+      api_host: POSTHOG_API_HOST,
       person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
       capture_pageview: false,
       capture_exceptions: true
