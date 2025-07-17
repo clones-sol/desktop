02c74e9cfc14002cde3b3d38dfa38da29c37e176
redDwarf03 <sylvain@archethic.net>
2025-07-17 10:48:44 +0200

    feat: :sparkles: Integrate file_saver package for script download functionality; update UI components to support downloading scripts in various formats.

--- clones/macos/Flutter/GeneratedPluginRegistrant.swift
+++ clones/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import file_saver
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
@@ -13,6 +14,7 @@ import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
