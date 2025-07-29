a00cd0528c9de0a0478a96dc658b5a7115b0e6bb
diamondly777 <lightbeam9811@gmail.com>
2025-07-29 17:36:40 -0400

    added referral domain models

---
+++ lib/domain/models/referral/referral_info.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'referral_info.freezed.dart';
+part 'referral_info.g.dart';
+
+@freezed
+class ReferralInfo with _$ReferralInfo {
+  const factory ReferralInfo({
+    required String referralCode,
+    required String referralLink,
+    required String walletAddress,
+    required int totalReferrals,
+    required double totalRewards,
+    required bool isActive,
+    required DateTime createdAt,
+    DateTime? lastUpdated,
+  }) = _ReferralInfo;
+
+  factory ReferralInfo.fromJson(Map<String, dynamic> json) =>
+      _$ReferralInfoFromJson(json);
+} 
