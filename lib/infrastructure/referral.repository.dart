5b469afa6d801d067af5aa2e94d6f0f7b1b6048a
diamondly777 <lightbeam9811@gmail.com>
2025-07-29 17:37:34 -0400

    added referral infrastructure layer

---
+++ lib/infrastructure/referral.repository.dart
@@ -0,0 +1,42 @@
+import 'package:clones_desktop/domain/models/api/referral_api.dart';
+import 'package:clones_desktop/domain/models/referral/referral_info.dart';
+import 'package:clones_desktop/utils/api_client.dart';
+
+abstract class ReferralRepository {
+  Future<CreateReferralResponse> createReferral(String walletAddress);
+  Future<GetReferralInfoResponse> getReferralInfo(String walletAddress);
+}
+
+class ReferralRepositoryImpl implements ReferralRepository {
+  final ApiClient _apiClient;
+
+  ReferralRepositoryImpl(this._apiClient);
+
+  @override
+  Future<CreateReferralResponse> createReferral(String walletAddress) async {
+    try {
+      final request = CreateReferralRequest(walletAddress: walletAddress);
+      final response = await _apiClient.post<Map<String, dynamic>>(
+        '/referral/create',
+        data: request.toJson(),
+      );
+
+      return CreateReferralResponse.fromJson(response);
+    } catch (e) {
+      throw Exception('Failed to create referral: $e');
+    }
+  }
+
+  @override
+  Future<GetReferralInfoResponse> getReferralInfo(String walletAddress) async {
+    try {
+      final response = await _apiClient.get<Map<String, dynamic>>(
+        '/referral/info/$walletAddress',
+      );
+
+      return GetReferralInfoResponse.fromJson(response);
+    } catch (e) {
+      throw Exception('Failed to get referral info: $e');
+    }
+  }
+} 
